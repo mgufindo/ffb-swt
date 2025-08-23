18fe498017cd054b05b3b56b8910aa669d465925
mgufindo <mgufindo@gmail.com>
2025-08-23 16:47:18 +0700

    add config

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,47 @@
-// vite.config.ts
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({
-  plugins: [react(),  tailwindcss() ],
-  server: {
-    host: '0.0.0.0',
-    port: 3000
-  },
-  preview: {
-    port: 3000
-  },
-  define: {
-    global: 'globalThis',
-  },
-  resolve: {
-    alias: {
-      buffer: 'buffer',
-      process: 'process/browser',
+export default defineConfig(({ mode }) => {
+  // ambil env sesuai mode (development / production)
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+      {
+        name: "html-transform",
+        transformIndexHtml(html) {
+          return html.replace(
+            /<title>(.*?)<\/title>/,
+            `<title>${env.VITE_APP_NAME}</title>`
+          );
+        },
+      },
+    ],
+    server: {
+      host: "0.0.0.0",
+      port: env.VITE_PORT ? parseInt(env.VITE_PORT) : 3000,
     },
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      define: {
-        global: 'globalThis',
+    preview: {
+      port: env.VITE_PREVIEW_PORT ? parseInt(env.VITE_PREVIEW_PORT) : 3000,
+    },
+    define: {
+      global: "globalThis",
+    },
+    resolve: {
+      alias: {
+        buffer: "buffer",
+        process: "process/browser",
       },
     },
-  },
-  
-})
+    optimizeDeps: {
+      esbuildOptions: {
+        define: {
+          global: "globalThis",
+        },
+      },
+    },
+  };
+});
